4ddf752313ee06a467f4bd8f77e7d05a184093a6
Renkiro <renchubongi@gmail.com>
2024-11-25 02:22:07 -0300

    healthbar player fix

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,18 @@ public class Player : MonoBehaviour
     public static Player Instance { get; private set; }
 
     public float health = 100f;
+    private float maxHealth;
 
+    [SerializeField] private GameObject healthBar;
+    private HealthBar healthBarScript;
+
+    private void Start()
+    {
+        maxHealth = health;
+        if (healthBar.GetComponent<HealthBar>()!= null)
+            healthBarScript = healthBar.GetComponent<HealthBar>();
+    }
+    
     private void Awake()
     {
         if (Instance == null)
@@ -17,6 +28,12 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    
+    private void Update()
+    {
+        if (healthBarScript != null)
+            healthBarScript.updateHealthBar(health, maxHealth);
     }
 
     public void TakeDamage(float damage)
